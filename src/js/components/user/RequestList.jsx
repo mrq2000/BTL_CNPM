42b8dd94cc742152cb76e63c860f58479cea4991
HieuPham2000 <hieu.pt183535@gmail.com>
2020-12-08 09:55:14 +0700

    add manage info, request list

--- src/js/components/user/RequestList.jsx
+++ src/js/components/user/RequestList.jsx
@@ -1,13 +1,57 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 import {
-  Table, TableContainer, TableHead, TableRow, TableCell, Paper, LinearProgress, TableBody,
+  Table, TableContainer, TableHead, TableRow, TableCell, Paper, LinearProgress, TableBody, Button,
+  Dialog, DialogTitle, DialogContent,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import useGetRequest from '../../data/useGetRequest';
 import requestStatus from '../../enums/requestStatus';
 
+const useStyles = makeStyles((theme) => ({
+  title: {
+    fontWeight: 'bold',
+    minWidth: theme.spacing(3),
+  },
+}));
+
+const FeedBack = ({ data }) => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Dialog onClose={() => setOpen(false)} open={open} fullWidth maxWidth="md">
+        <DialogTitle>Trả lời từ Tổ Trưởng</DialogTitle>
+        <DialogContent>
+          <TableRow>
+            <TableCell className={classes.title}>
+              Nội dung:
+            </TableCell>
+            <TableCell>
+              {data.content}
+            </TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell className={classes.title}>
+              Ngày trả lời:
+            </TableCell>
+            <TableCell>
+              {data.datetime}
+            </TableCell>
+          </TableRow>
+        </DialogContent>
+      </Dialog>
+
+      <Button onClick={() => setOpen(true)}>Chi tiết</Button>
+    </>
+  );
+};
+
 const RequestList = () => {
   const { isLoading, data, error } = useGetRequest();
+
   if (isLoading) return <LinearProgress style={{ width: '100%' }} />;
   if (error) return <>Error</>;
 
@@ -18,6 +62,7 @@ const RequestList = () => {
         <TableCell size="medium" align="left">Nội dung</TableCell>
         <TableCell size="medium" align="left">Ngày đăng</TableCell>
         <TableCell size="medium" align="left">Trạng thái</TableCell>
+        <TableCell size="medium" align="left" />
       </TableRow>
     </TableHead>
   );
@@ -32,6 +77,13 @@ const RequestList = () => {
               <TableCell size="small" align="left">{data[id].content}</TableCell>
               <TableCell size="small" align="left">{data[id].datetime}</TableCell>
               <TableCell size="small" align="left">{requestStatus.getTitleStatus(data[id].state)}</TableCell>
+              <TableCell size="medium" align="left">
+                {
+                  data[id].state === requestStatus.DONE
+                    ? <FeedBack data={data[id].feedback} />
+                    : null
+                }
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
