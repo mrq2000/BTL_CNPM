7bf9b9509878d2fd19512056a5734ba3f0e7c0ec
HieuPham2000 <hieu.pt183535@gmail.com>
2020-12-13 17:44:37 +0700

    Add logo

--- src/js/components/common/Header.jsx
+++ src/js/components/common/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Typography, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import logo from './logo.jpg';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -12,17 +13,21 @@ const useStyles = makeStyles((theme) => ({
   italic: {
     fontStyle: 'italic',
   },
+  logo: {
+    borderRadius: '50%',
+    maxWidth: 80,
+  }
 }));
 
 const Header = () => {
   const APP_NAME = 'HỆ THỐNG QUẢN LÝ KIẾN NGHỊ - PHẢN ÁNH';
-  const LOCATION_ADDRESS = 'tổ  dân phố 7, phường La Khê, quận Hà Đông, thành phố Hà Nội';
+  const LOCATION_ADDRESS = 'Tổ  dân phố 7, phường La Khê, quận Hà Đông, thành phố Hà Nội';
   const classes = useStyles();
 
   return (
     <Box className={classes.container} display="flex">
-      <Box>
-        Logo
+      <Box display="flex" justifyContent="center" flex={0.3} >
+        <img src={logo} className={classes.logo}/>
       </Box>
       <Box display="flex" justify="center" alignItems="center" flexDirection="column" flex={1}>
         <Typography variant="h4" align="center">
