f5ce35f1662cae800122b6da33e38379f197c972
MTuan247 <71715391+MTuan247@users.noreply.github.com>
2020-12-14 23:44:08 +0700

    add dayjs

--- src/js/helpers/dayjs.js
+++ src/js/helpers/dayjs.js
@@ -1,4 +1,4 @@
 import dayjs from 'dayjs';
 
-export const formatDateTime = (dateTime) => dayjs(dateTime).format('HH:mm DD/MM/YYYY');
-export const timeNow = formatDateTime(Date.now());
+export const formatDateTime = (dateTime) => dayjs(Date(dateTime)).format('HH:mm DD/MM/YYYY');
+export const timeNow = Date.now();
