55ac64fa5487d7492d3169a4202b08e214faf483
MTuan247 <71715391+MTuan247@users.noreply.github.com>
2020-12-14 23:47:45 +0700

    add SignIn

--- src/js/components/auth/SignIn.jsx
+++ src/js/components/auth/SignIn.jsx
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -101,10 +99,6 @@ export default function SignIn() {
             error={!!errors.password}
             helperText={errors.password ? errors.password.message : ''}
             autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
           />
           <Button
             type="submit"
