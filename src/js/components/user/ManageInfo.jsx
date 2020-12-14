f0685d181dcac22542a5dd00d83c9e4dcb2682b2
HieuPham2000 <hieu.pt183535@gmail.com>
2020-12-14 21:45:59 +0700

    modify assets/index.html, Root, ManageInfo, SignIn

--- src/js/components/user/ManageInfo.jsx
+++ src/js/components/user/ManageInfo.jsx
@@ -11,6 +11,12 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 'bold',
     marginRight: theme.spacing(2),
   },
+  img: {
+    marginRight: theme.spacing(3),
+    height: theme.spacing(50),
+    width: theme.spacing(40),
+    objectFit: 'cover',
+  },
 }));
 
 const AdminInfo = () => {
@@ -24,68 +30,72 @@ const AdminInfo = () => {
     <>
       {
         Object.keys(admins).map((id) => (
-          <Box display="flex" flexDirection="column" key={id} mt={3}>
-            <Box display="flex">
-              <Typography className={classes.title}>
-                Tên:
-              </Typography>
-              <Typography>
-                {admins[id].name}
-              </Typography>
-            </Box>
+          <Box display="flex" flexDirection="row" mt={3}>
+            <img alt="logo" src={admins[id].img} className={classes.img} />
 
-            <Box display="flex">
-              <Typography className={classes.title}>
-                Giới Tính:
-              </Typography>
-              <Typography>
-                {admins[id].gender}
-              </Typography>
-            </Box>
+            <Box display="flex" flexDirection="column">
+              <Box display="flex">
+                <Typography className={classes.title}>
+                  Tên:
+                </Typography>
+                <Typography>
+                  {admins[id].name}
+                </Typography>
+              </Box>
 
-            <Box display="flex">
-              <Typography className={classes.title}>
-                Địa chỉ:
-              </Typography>
-              <Typography>
-                {admins[id].address}
-              </Typography>
-            </Box>
+              <Box display="flex">
+                <Typography className={classes.title}>
+                  Giới Tính:
+                </Typography>
+                <Typography>
+                  {admins[id].gender}
+                </Typography>
+              </Box>
 
-            <Box display="flex">
-              <Typography className={classes.title}>
-                Ngày sinh:
-              </Typography>
-              <Typography>
-                {admins[id].birthday}
-              </Typography>
-            </Box>
+              <Box display="flex">
+                <Typography className={classes.title}>
+                  Địa chỉ:
+                </Typography>
+                <Typography>
+                  {admins[id].address}
+                </Typography>
+              </Box>
 
-            <Box display="flex">
-              <Typography className={classes.title}>
-                Số điện thoại:
-              </Typography>
-              <Typography>
-                {admins[id].phone}
-              </Typography>
-            </Box>
+              <Box display="flex">
+                <Typography className={classes.title}>
+                  Ngày sinh:
+                </Typography>
+                <Typography>
+                  {admins[id].birthday}
+                </Typography>
+              </Box>
 
-            <Box display="flex">
-              <Typography className={classes.title}>
-                Email:
-              </Typography>
-              <Typography>
-                {admins[id].email}
-              </Typography>
-            </Box>
+              <Box display="flex">
+                <Typography className={classes.title}>
+                  Số điện thoại:
+                </Typography>
+                <Typography>
+                  {admins[id].phone}
+                </Typography>
+              </Box>
 
-            <Box display="flex">
-              <Typography className={classes.title}>
-                User Name:
-              </Typography>
-              <Typography>
-                {admins[id].username}
-              </Typography>
+              <Box display="flex">
+                <Typography className={classes.title}>
+                  Email:
+                </Typography>
+                <Typography>
+                  {admins[id].email}
+                </Typography>
+              </Box>
+
+              <Box display="flex">
+                <Typography className={classes.title}>
+                  User Name:
+                </Typography>
+                <Typography>
+                  {admins[id].username}
+                </Typography>
+              </Box>
             </Box>
           </Box>
         ))
